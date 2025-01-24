7657e1ad7edc88f2d8f7427a84436aaedfb2f8f9
elias <elias.zulauf@lernende.bbw.ch>
2025-01-24 13:55:43 +0100

    initial commit

---
+++ src/test/java/ch/bbw/m320/hanoi/HanoiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m320.hanoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
